ccdd63ce3c88132782764f8124473729ecfd4cfa
Rizky <cierizkynanda@gmail.com>
2024-07-18 17:44:27 +0700

    finish templating admin dan user

---
+++ resources/views/admin/jenis-surat/index.blade.php
@@ -0,0 +1,23 @@
+@extends('template.admin')
+
+@section('content')
+    <div class="dashboard-main-body">
+        <div class="d-flex flex-wrap align-items-center justify-content-between gap-3 mb-24">
+            <h6 class="fw-semibold mb-0">{{ $page }}</h6>
+        </div>
+        <div class="row gy-4 mt-1">
+            <div class="col-xxl-12 col-xl-12">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <div class="d-flex flex-wrap align-items-center justify-content-between">
+                        </div>
+                        <div class="d-flex flex-wrap align-items-center gap-2 mt-8">
+
+                        </div>
+                        <h4>Welcome!</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/template/admin/sidebar.blade.php
@@ -0,0 +1,33 @@
+<aside class="sidebar">
+    <button type="button" class="sidebar-close-btn">
+        <iconify-icon icon="radix-icons:cross-2"></iconify-icon>
+    </button>
+    <div>
+        <a href="javascript:void(0)" class="sidebar-logo">
+            <img src="https://www.unimed.ac.id/wp-content/uploads/2022/07/Unimed-2.svg" alt="site logo" class="light-logo">
+            <img src="https://www.unimed.ac.id/wp-content/uploads/2022/07/Unimed-2.svg" alt="site logo" class="dark-logo">
+            <img src="https://www.unimed.ac.id/wp-content/uploads/2022/07/Unimed-2.svg" alt="site logo" class="logo-icon">
+        </a>
+    </div>
+    @php
+        // Get the first segment of the URL
+        $url = request()->segment(2);
+    @endphp
+    <div class="sidebar-menu-area">
+        <ul class="sidebar-menu" id="sidebar-menu">
+            <li class="sidebar-menu-group-title">Menu</li>
+            <li class="{{ ($url == "") ? "active-page" : "" }}">
+                <a href="{{ route('admin.index') }}" class="{{ ($url == "") ? "active-page" : "" }}">
+                    <iconify-icon icon="ion:home" class="menu-icon"></iconify-icon>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin.jenis-surat.index') }}">
+                    <iconify-icon icon="ion:mail" class="menu-icon"></iconify-icon>
+                    <span>Jenis Surat</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</aside>
