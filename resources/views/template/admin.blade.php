25cc59c07fc05018dd62cc2abd83c4596da5527e
Rizky <cierizkynanda@gmail.com>
2024-07-18 20:09:36 +0700

    fix: folder assets ke public

--- resources/views/template/admin.blade.php
+++ resources/views/template/admin.blade.php
@@ -5,32 +5,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wowdash - Bootstrap 5 Admin Dashboard HTML Template</title>
-    <link rel="icon" type="image/png" href="{{ asset('assets/images/favicon.png') }} " sizes="16x16">
+    <title>{{ env('APP_NAME') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/logo.png') }} " sizes="16x16">
     <!-- remix icon font css  -->
-    <link rel="stylesheet" href="{{ asset('storage//assets/css/remixicon.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/css/remixicon.css') }} ">
     <!-- BootStrap css -->
-    <link rel="stylesheet" href="{{ asset('storage//assets/css/lib/bootstrap.min.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/css/lib/bootstrap.min.css') }} ">
     <!-- Apex Chart css -->
-    <link rel="stylesheet" href="{{ asset('storage//assets/css/lib/apexcharts.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/css/lib/apexcharts.css') }} ">
     <!-- Data Table css -->
-    <link rel="stylesheet" href="{{ asset('storage//assets/css/lib/dataTables.min.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/css/lib/dataTables.min.css') }} ">
     <!-- Text Editor css -->
-    <link rel="stylesheet" href="{{ asset('storage//assets/css/lib/editor-katex.min.css') }} ">
-    <link rel="stylesheet" href="{{ asset('storage//assets/css/lib/editor.atom-one-dark.min.css') }} ">
-    <link rel="stylesheet" href="{{ asset('storage//assets/css/lib/editor.quill.snow.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/css/lib/editor-katex.min.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/css/lib/editor.atom-one-dark.min.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/css/lib/editor.quill.snow.css') }} ">
     <!-- Date picker css -->
-    <link rel="stylesheet" href="{{ asset('storage//assets/css/lib/flatpickr.min.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/css/lib/flatpickr.min.css') }} ">
     <!-- Calendar css -->
-    <link rel="stylesheet" href="{{ asset('storage//assets/css/lib/full-calendar.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/css/lib/full-calendar.css') }} ">
     <!-- Vector Map css -->
-    <link rel="stylesheet" href="{{ asset('storage//assets/css/lib/jquery-jvectormap-2.0.5.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/css/lib/jquery-jvectormap-2.0.5.css') }} ">
     <!-- Popup css -->
-    <link rel="stylesheet" href="{{ asset('storage//assets/css/lib/magnific-popup.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/css/lib/magnific-popup.css') }} ">
     <!-- Slick Slider css -->
-    <link rel="stylesheet" href="{{ asset('storage//assets/css/lib/slick.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/css/lib/slick.css') }} ">
     <!-- main css -->
-    <link rel="stylesheet" href="{{ asset('storage//assets/css/style.css') }} ">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }} ">
 </head>
 <body>
 @include('template.admin.sidebar')
@@ -44,28 +44,28 @@
 </main>
 
 <!-- jQuery library js -->
-<script src="{{ asset('storage/assets/js/lib/jquery-3.7.1.min.js') }}"></script>
+<script src="{{ asset('assets/js/lib/jquery-3.7.1.min.js') }}"></script>
 <!-- Bootstrap js -->
-<script src="{{ asset('storage/assets/js/lib/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/js/lib/bootstrap.bundle.min.js') }}"></script>
 <!-- Apex Chart js -->
-<script src="{{ asset('storage/assets/js/lib/apexcharts.min.js') }}"></script>
+<script src="{{ asset('assets/js/lib/apexcharts.min.js') }}"></script>
 <!-- Data Table js -->
-<script src="{{ asset('storage/assets/js/lib/dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/js/lib/dataTables.min.js') }}"></script>
 <!-- Iconify Font js -->
-<script src="{{ asset('storage/assets/js/lib/iconify-icon.min.js') }}"></script>
+<script src="{{ asset('assets/js/lib/iconify-icon.min.js') }}"></script>
 <!-- jQuery UI js -->
-<script src="{{ asset('storage/assets/js/lib/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('assets/js/lib/jquery-ui.min.js') }}"></script>
 <!-- Vector Map js -->
-<script src="{{ asset('storage/assets/js/lib/jquery-jvectormap-2.0.5.min.js') }}"></script>
-<script src="{{ asset('storage/assets/js/lib/jquery-jvectormap-world-mill-en.js') }}"></script>
+<script src="{{ asset('assets/js/lib/jquery-jvectormap-2.0.5.min.js') }}"></script>
+<script src="{{ asset('assets/js/lib/jquery-jvectormap-world-mill-en.js') }}"></script>
 <!-- Popup js -->
-<script src="{{ asset('storage/assets/js/lib/magnifc-popup.min.js') }}"></script>
+<script src="{{ asset('assets/js/lib/magnifc-popup.min.js') }}"></script>
 <!-- Slick Slider js -->
-<script src="{{ asset('storage/assets/js/lib/slick.min.js') }}"></script>
+<script src="{{ asset('assets/js/lib/slick.min.js') }}"></script>
 <!-- main js -->
-<script src="{{ asset('storage/assets/js/app.js') }}"></script>
+<script src="{{ asset('assets/js/app.js') }}"></script>
 
-<script src="{{ asset('storage/assets/js/homeOneChart.js') }}"></script>
+<script src="{{ asset('assets/js/homeOneChart.js') }}"></script>
 
 </body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,25 +8,25 @@
     <title>Wowdash - Bootstrap 5 Admin Dashboard HTML Template</title>
     <link rel="icon" type="image/png" href="{{ asset('storage/logo.png') }}" sizes="16x16">
     <!-- remix icon font css  -->
-    <link rel="stylesheet" href="{{ asset('storage/assets/css/remixicon.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/remixicon.css') }}">
     <!-- BootStrap css -->
-    <link rel="stylesheet" href="{{ asset('storage/assets/css/lib/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/lib/bootstrap.min.css') }}">
     <!-- main css -->
-    <link rel="stylesheet" href="{{ asset('storage/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 <body>
 
 <section class="auth bg-base d-flex flex-wrap">
     <div class="auth-left d-lg-block d-none">
         <div class="d-flex align-items-center flex-column h-100 justify-content-center">
-            <img src="{{ asset('storage/assets/images/auth/auth-img.png') }}" alt="">
+            <img src="{{ asset('assets/images/auth/auth-img.png') }}" alt="">
         </div>
     </div>
     <div class="auth-right py-32 px-24 d-flex flex-column justify-content-center">
         <div class="max-w-464-px mx-auto w-100">
             <div>
                 <a href="javascript:void(0)" class="mb-40 max-w-290-px">
-                    <img src="{{ asset('storage/logo.png') }}" alt="">
+                    <img src="{{ asset('logo.png') }}" alt="">
                 </a>
                 <h4 class="mb-12">Login Dengan Akun SSO Anda</h4>
             </div>
@@ -43,13 +43,13 @@
 </section>
 
 <!-- jQuery library js -->
-<script src="{{ asset('storage/assets/js/lib/jquery-3.7.1.min.js') }}"></script>
+<script src="{{ asset('assets/js/lib/jquery-3.7.1.min.js') }}"></script>
 <!-- Bootstrap js -->
-<script src="{{ asset('storage/assets/js/lib/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('storage/assets/js/lib/iconify-icon.min.js') }}"></script>
+<script src="{{ asset('assets/js/lib/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/js/lib/iconify-icon.min.js') }}"></script>
 
 <!-- main js -->
-<script src="{{ asset('storage/assets/js/app.js') }}"></script>
+<script src="{{ asset('assets/js/app.js') }}"></script>
 
 </script>
 
