3f9f84cd34efb186bc65219a0712f9333e67637f
ahps <akbarputrasrg@unimed.ac.id>
2024-07-20 10:19:51 +0700

    fix: HasFingerprint.php variable issue, move all variables from HasFingerprint.php to Model

--- app/Traits/Model/HasFingerprint.php
+++ app/Traits/Model/HasFingerprint.php
@@ -17,27 +17,6 @@ use Illuminate\Database\Eloquent\Model;
 trait HasFingerprint
 {
     /**
-     * Table column name of the CREATED_BY
-     *
-     * @var string
-     */
-    protected string $CREATED_BY;
-
-    /**
-     * Table column name of the UPDATED_BY
-     *
-     * @var string
-     */
-    protected string $UPDATED_BY;
-
-    /**
-     * Table column name of the DELETED_BY
-     *
-     * @var string
-     */
-    protected string $DELETED_BY;
-
-    /**
      * Boot the trait.
      *
      * @return void
@@ -46,38 +25,38 @@ trait HasFingerprint
     public static function bootHasFingerprint(): void
     {
         static::creating(function (Model $model) {
-            if (!$this->CREATED_BY) {
+            if (!$model->CREATED_BY) {
                 throw new Exception('CREATED_BY is not defined in Model class');
             }
-            if (!$this->UPDATED_BY) {
+            if (!$model->UPDATED_BY) {
                 throw new Exception('UPDATED_BY is not defined in Model class');
             }
 
             $userId = auth()->id();
-            $model->{$this->CREATED_BY} = $userId;
-            $model->{$this->UPDATED_BY} = $userId;
+            $model->{$model->CREATED_BY} = $userId;
+            $model->{$model->UPDATED_BY} = $userId;
         });
 
         static::updating(function (Model $model) {
-            if (!$this->UPDATED_BY) {
+            if (!$model->UPDATED_BY) {
                 throw new Exception('UPDATED_BY is not defined in Model class');
             }
 
-            $model->{$this->UPDATED_BY} = auth()->id();
+            $model->{$model->UPDATED_BY} = auth()->id();
         });
 
         static::deleting(function (Model $model) {
             if (!method_exists($model, 'trashed')) {
                 return;
             }
-            if (!$this->DELETED_BY) {
+            if (!$model->DELETED_BY) {
                 throw new Exception('DELETED_BY is not defined in Model class');
             }
 
             // when deleting record, we can't add the deleted_by directly
             // because deleting record is triggered after the record is deleted (soft delete)
             // so, we need to save it again after the deletion process
-            $model->{$this->DELETED_BY} = auth()->id();
+            $model->{$model->DELETED_BY} = auth()->id();
             $model->save();
         });
     }
