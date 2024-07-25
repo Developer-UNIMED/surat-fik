39b154ce8f01047285c65f72970ae7e499fa054e
Rizky <cierizkynanda@gmail.com>
2024-07-25 09:38:22 +0700

    delete route api

--- routes/api/v1/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-require __DIR__ . "/auth/routes.php";
-
-Route::get('/ping', function () {
-    return response()->json(['message' => 'pong']);
-});
